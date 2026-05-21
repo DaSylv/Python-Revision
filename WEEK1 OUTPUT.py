948b6eb0b58933891221a6a4ea4e9f844e5427a1
rtrda <rtrdana@gmail.com>
2026-05-21 20:34:50 +0100

    added a code to display escape charachters Added a code to display an ASCII art robot

---
+++ WEEK1 OUTPUT.py
@@ -0,0 +1,19 @@
+# print ("Welcome to QHO426")
+# print("In week 1 we will learn...")
+
+# #Display escape characters
+# print("\n Display a new line")
+# print("\t Display a tab spaces")
+# print("\\ Display a back slash")
+# print("\"Display a double quote")
+# print("\' Display a single quote")
+#
+#
+# print("I'm a programming")
+
+#ASCI Art
+#Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
