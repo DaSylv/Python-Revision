43b771ac0a8dfc31966ace10e8346279975db820
rtrda <rtrdana@gmail.com>
2026-05-24 16:26:23 +0100

    if else function input with modulo operator

---
+++ Week 2/modulo_operator.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a whole number.\n"))
+
+#verificam daca restul impartirii este egal cu 2
+if number % 2 == 0:
+    print(f"The number {number} is an even number.")
+
+else:
+    print(f"The number {number} is an odd number.")
