c74800d5412654f690f0eb7397d777db7403a389
rtrda <rtrdana@gmail.com>
2026-05-24 15:21:14 +0100

    if else function input

--- Week 2/if function.py
+++ Week 2/if function.py
@@ -1,11 +1,19 @@
-age = int(input("Enter your age:"))
-if age < 12 :
-    ticket_price = 0
-    print("Entry is free")
+# age = int(input("Enter your age:"))
+# if age < 12 :
+#     ticket_price = 0
+#     print("Entry is free")
+#
+# elif age > 65 :
+#     ticket_price = 10
+#     print("Senior Discount")
+# else :
+#     ticket_price = 15
+#     print(f"ticket is {ticket_price}")
 
-elif age > 65 :
-    ticket_price = 10
-    print("Senior Discount")
-else :
-    ticket_price = 15
-    print(f"ticket is {ticket_price}")
+#adventure book
+book = input("What type of book is this")
+if book == "adventure" :
+           print("I like adventure books!")
+
+print("Finished reading book")
+
---
+++ Week 2/if_statement.py
@@ -0,0 +1,7 @@
+activity = input("Please enter the activity to be performed.")
+if activity == "Calculate":
+    print("Performing calculation...")
+else :
+    print("No calculations...")
+
+print("Activity completed...!")
