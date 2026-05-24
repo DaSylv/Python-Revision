378655a59dc0032243930cb2872b7ec7e2a39230
rtrda <rtrdana@gmail.com>
2026-05-24 16:01:34 +0100

    if else function input

---
+++ Week 2/if_elif_else.py
@@ -0,0 +1,12 @@
+entity = input("What is your entity?")
+if entity == "Human":
+    print("You are a human!")
+
+elif entity == "robot":
+    print("You are a robot!")
+
+elif entity == "Animal":
+    print("You are an animal!")
+else :
+    print("I do not know what you are!")
+print("Analysis completed...")
