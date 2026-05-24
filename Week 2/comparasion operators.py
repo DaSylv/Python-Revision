8667eb097c79e919ec7e15c98436fac5ce1a5a4a
rtrda <rtrdana@gmail.com>
2026-05-24 16:44:05 +0100

    Demonstrate the use of comparasion operators

---
+++ Week 2/comparasion operators.py
@@ -0,0 +1,11 @@
+first_number = int(input("Please enter the first number:"))
+second_number = int(input("Please enter the second number:"))
+
+if first_number < second_number:
+    print("The first number is the smallest..")
+
+elif second_number > first_number:
+    print("The second number is the biggest..")
+
+else:
+    print("Both are equal")
