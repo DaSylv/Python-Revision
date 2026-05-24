aec234e81b19f0b3df1df6e8557a9d178b243c58
rtrda <rtrdana@gmail.com>
2026-05-24 16:10:44 +0100

    if else function input

---
+++ Week 2/robotMaze_if,elif.py
@@ -0,0 +1,17 @@
+direction = input("In which direction should I go?")
+if direction == "right":
+    print("Robot will go to the right")
+
+elif direction == "left":
+    print("Robot will go to the left")
+
+elif direction == "down":
+    print("Robot will go to the down")
+
+elif direction == "up":
+    print("The robot will go to the up")
+
+else :
+    print("I do not know where to go...")
+
+print("Game on...")
