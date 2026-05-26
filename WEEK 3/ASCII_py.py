04c2bf09131779e4af61fdf8d6c005bc9857ee69
rtrda <rtrdana@gmail.com>
2026-05-26 10:08:40 +0100

    demonstrate the use of an while loop with ASCII

---
+++ WEEK 3/ASCII_py.py
@@ -0,0 +1,12 @@
+# we ask the user how many lines of battery want to charge
+total_bars = int(input("How many bars should be charged?\n"))
+
+# we star count from 0
+bars_charged = 0
+# rulam bucla while pana cand incarcam toate liniile
+while bars_charged > bars_charged:
+    bars_charged = bars_charged + 1
+
+# inmultim caracterul special ! cu valoarea curenta
+    print(f" Charging: {'!' * bars_charged}")
+print ("The battery is fully charged")        
