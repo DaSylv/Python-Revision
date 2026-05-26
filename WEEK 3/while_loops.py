eec585fc62f063f8cfe7ab1a1e86b5f688309ac5
rtrda <rtrdana@gmail.com>
2026-05-26 09:34:04 +0100

    demonstrate the use of an simple while loop

---
+++ WEEK 3/while_loops.py
@@ -0,0 +1,13 @@
+#We ask the user and save the number
+total_apples_to_remove = int(input("How many apples should I remove?"))
+
+#Cream o variabila care urmareste nr de mere
+removed_apples = 0
+
+#we use a while loop
+while removed_apples < total_apples_to_remove:
+    #we show the message
+    print("Removed apple")
+
+    #marim variabila cu 1
+    removed_apples = removed_apples + 1
