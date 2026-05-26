b4bde07cba5d3d71e97423fe7419c54f249c6ae1
rtrda <rtrdana@gmail.com>
2026-05-26 08:10:26 +0100

    demonstrate the use of a logical operator/AND

---
+++ Week 2/and_condition.py
@@ -0,0 +1,8 @@
+print("What did I hear?")
+hear = input()
+print("What did I see?")
+see = input()
+if (hear == "grrrr") and (see == "two red eyes"):
+    print("There is a scary creature.I should get out of there!")
+else :
+    print("I'm a little scared but I will continue")
