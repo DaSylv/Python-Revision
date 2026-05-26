e3357a06764563e6a2d2222f5023fa031269cedc
rtrda <rtrdana@gmail.com>
2026-05-26 20:02:43 +0100

    demonstrate the use of a while loop with len function

---
+++ WEEK 3/len_function.py
@@ -0,0 +1,10 @@
+# we ask the user and save the text
+phrase = input("Please enter a phrase:\n")
+
+#we use len() function to found how many characters we have
+length_of_phrase = len(phrase)
+
+#we multiply text"hi"with the no of characters
+#we add space after "hi"
+result = "Hi " *length_of_phrase
+print(result)
