f368f745e5b25eed38af852da26403e56b45453d
rtrda <rtrdana@gmail.com>
2026-05-28 18:13:55 +0100

    demonstrate how to index a list

--- Week 5/simple_lists.py
+++ Week 5/simple_lists.py
@@ -6,3 +6,18 @@ def run_task1():
     print(nav_steps)
 if __name__ == "__main__":
     run_task1()
+
+def movements():
+    path = ["Move Forward",10, "Move Backward",5, "Move Left",3,"Move Right",1]
+    return path
+
+def run_task2():
+    print("Moving...")
+    path = movements()
+    for i in range(0, len(path),2):
+        direction = path[i]
+        steps = path[i+1]
+        print(f" {direction} for {steps} steps")
+
+if __name__ == "__main__":
+    run_task2()
