5fa3539b0bfdcc681c96fb0a6b284d90600df37e
rtrda <rtrdana@gmail.com>
2026-05-28 17:49:05 +0100

    demonstrate a simple list

---
+++ Week 5/simple_lists.py
@@ -0,0 +1,8 @@
+def directions():
+    steps = ["Move Forward","Move Backward","Move Right","Move Left"]
+    return steps
+def run_task1():
+    nav_steps = directions()
+    print(nav_steps)
+if __name__ == "__main__":
+    run_task1()
