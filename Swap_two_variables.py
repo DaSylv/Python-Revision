15b37743aea5de96b707b4822bbdeb98c10d0132
rtrda <rtrdana@gmail.com>
2026-06-11 20:12:13 +0100

    swap two variables

---
+++ Swap_two_variables.py
@@ -0,0 +1,13 @@
+# Input two variables
+a = input("Enter the value of the first variable (a):")
+b = input("Enter the value of the second variable (b):")
+
+#Display the original values
+print(f"Original values: a = {a}, b = {b}")
+
+temp = a
+a = b
+b = temp
+
+#Display the swapped values
+print(f"Swapped values: a = {a}, b = {b}")
