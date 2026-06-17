6dac918a5b79ff26591b0f517acbbc65ee66ee57
rtrda <rtrdana@gmail.com>
2026-06-17 10:33:23 +0100

    Check if a number is an Armnstrong number in a interval

---
+++ armnstrong_number_in_a_interval.py
@@ -0,0 +1,17 @@
+# input the interval from the user
+lower = int(input("Enter the lower limit of the interval: "))
+upper = int(input("Enter the upper limit of the interval: "))
+
+for num in range(lower,upper +1):
+     order = len(str(num)) #find the number of digits in 'num'
+     temp_num = num
+     sum = 0
+
+     while temp_num > 0:
+         digit = temp_num % 10
+         sum += digit ** order
+         temp_num //= 10
+
+     #Check if 'num' is an ARmnstrong number
+     if num == sum:
+         print(num)
